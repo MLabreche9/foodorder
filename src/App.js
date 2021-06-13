e70fbcb8bb91a78331f7734d9b54804b6cbca6c5
Meghan La Breche <Megavictoria@gmail.com>
2021-06-12 20:37:37 -0700

    Header and Meal Items

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import Header from './componenets/Layout/Header';
+import Meals from './componenets/Meals/Meals';
+
+function App() {
+	return (
+		<Fragment>
+			<Header />
+			<main>
+				<Meals />
+			</main>
+		</Fragment>
+	);
+}
+
+export default App;
