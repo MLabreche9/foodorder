2a1740648fef581c663dcf6e1e53aa6087760a09
Meghan La Breche <Megavictoria@gmail.com>
2021-06-13 18:22:52 -0700

    Meal Amounts & Cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import Header from './componenets/Layout/Header';
 import Meals from './componenets/Meals/Meals';
+import Cart from './componenets/Cart/Cart';
 
 function App() {
 	return (
 		<Fragment>
+			<Cart />
 			<Header />
 			<main>
 				<Meals />
